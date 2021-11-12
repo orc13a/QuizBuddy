122c50e40c379960a39abaffa58b1b3b7ecfa495
Oliver Christensen <orc13a@live.dk>
2021-11-12 18:50:29 +0100

    teams page

--- client/src/components/teacher/Teams/Teams.js
+++ client/src/components/teacher/Teams/Teams.js
@@ -1,21 +1,40 @@
-import { Card, Col, Grid, Text, useMantineTheme } from "@mantine/core";
+import { Card, Col, Grid, Text } from "@mantine/core";
 import Navbar from "../Navbar/Navbar";
 
 export default function TeacherTeams() {
-    const theme = useMantineTheme();
 
     return(
         <>
             <Navbar>
                 <Grid gutter="xl">
-                    <Col span={3}>
+                    <Col span={12} sm={3} md={4} lg={3}>
                         <Card className="teamCard" withBorder>
                             <Text weight={500}>Hold 1</Text>
                         </Card>
                     </Col>
-                    <Col span={3}>
+                    <Col span={12} sm={3} md={4} lg={3}>
                         <Card className="teamCard" withBorder>
                             <Text weight={500}>Hold 2</Text>
+                        </Card>
+                    </Col>
+                    <Col span={12} sm={3} md={4} lg={3}>
+                        <Card className="teamCard" withBorder>
+                            <Text weight={500}>Hold 3</Text>
+                        </Card>
+                    </Col>
+                    <Col span={12} sm={3} md={4} lg={3}>
+                        <Card className="teamCard" withBorder>
+                            <Text weight={500}>Hold 4</Text>
+                        </Card>
+                    </Col>
+                    <Col span={12} sm={3} md={4} lg={3}>
+                        <Card className="teamCard" withBorder>
+                            <Text weight={500}>Hold 5</Text>
+                        </Card>
+                    </Col>
+                    <Col span={12} sm={3} md={4} lg={3}>
+                        <Card className="teamCard" withBorder>
+                            <Text weight={500}>Hold 6</Text>
                         </Card>
                     </Col>
                 </Grid>
--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,13 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { MantineProvider } from '@mantine/core';
 
-const getTheme = () => {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        return 'dark';
-    } else {
-        return 'light';
-    }
-}
+// const getTheme = () => {
+//     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//         return 'dark';
+//     } else {
+//         return 'light';
+//     }
+// }
 
 ReactDOM.render(
     <React.StrictMode>
